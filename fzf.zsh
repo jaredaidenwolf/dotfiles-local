c4d007488bcc03822494d7901a4d0ab09b8da6c6
Jared Aiden Wolf <jared@jaredaidenwolf.com>
2024-10-01 11:23:28 -0400

    Updated aliases, added chromedriver to brew installs

--- fzf.zsh
+++ fzf.zsh
@@ -4,12 +4,4 @@ if [[ ! "$PATH" == */Users/jaredaidenwolf/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/Users/jaredaidenwolf/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/Users/jaredaidenwolf/.fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/Users/jbranam/.fzf/shell/key-bindings.zsh"
-
 source <(fzf --zsh)
